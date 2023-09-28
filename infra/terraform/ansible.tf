6f3ae28e49bc6fe999afbd1c542139fc7c818177
Sami Honkasalo <honkasalo.sami@gmail.com>
2023-09-28 19:27:16 +0300

    Ansible WiP 🔧

--- infra/terraform/ansible.tf
+++ infra/terraform/ansible.tf
@@ -1,22 +1,34 @@
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/../ansible/inventory.tftpl", {
-    control_planes = proxmox_vm_qemu.k8s_control_plane
-    nodes          = proxmox_vm_qemu.k8s_node
-  })
-  filename = "${path.module}/../ansible/inventory"
+resource "ansible_host" "control_planes" {
+  for_each = proxmox_vm_qemu.k8s_control_plane
+  name     = each.value.default_ipv4_address
+  groups   = ["control_planes"]
+  variables = {
+    ansible_user                 = "saho"
+    ansible_ssh_private_key_file = "~/.ssh/pve"
+  }
+}
+
+resource "ansible_host" "nodes" {
+  for_each = proxmox_vm_qemu.k8s_node
+  name     = each.value.default_ipv4_address
+  groups   = ["nodes"]
+  variables = {
+    ansible_user                 = "saho"
+    ansible_ssh_private_key_file = "~/.ssh/pve"
+  }
 }
 
 
-resource "ansible_playbook" "common_control_planes" {
-  for_each  = var.control_planes
-  name      = each.value.name
-  playbook  = "${path.module}/../ansible/playbooks/common"
+resource "ansible_playbook" "control_planes_common" {
+  for_each  = proxmox_vm_qemu.k8s_control_plane
+  name      = each.value.default_ipv4_address
+  playbook  = "${path.module}/../ansible/playbooks/common.yaml"
   verbosity = 6
 }
 
-resource "ansible_playbook" "common_nodes" {
-  for_each  = var.nodes
-  name      = each.value.name
-  playbook  = "${path.module}/../ansible/playbooks/common"
-  verbosity = 6
-}
+# resource "ansible_playbook" "common_nodes" {
+#   for_each  = var.nodes
+#   name      = each.value.name
+#   playbook  = "${path.module}/../ansible/playbooks/common"
+#   verbosity = 6
+# }
