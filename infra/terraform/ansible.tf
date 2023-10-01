4a94908610deddbec75b3f3facf304ab018b7005
Sami Honkasalo <honkasalo.sami@gmail.com>
2023-10-01 10:33:08 +0300

    Update ansible playbooks

--- infra/terraform/ansible.tf
+++ infra/terraform/ansible.tf
@@ -19,30 +19,30 @@ resource "ansible_host" "nodes" {
 }
 
 
-resource "ansible_playbook" "control_planes_common" {
-  for_each   = ansible_host.control_planes
-  name       = each.value.name
-  groups     = each.value.groups
-  playbook   = "${path.module}/../ansible/playbooks/common.yaml"
-  replayable = true
-  extra_vars = {
-    ansible_host                 = each.value.name
-    ansible_groups               = join(",", each.value.groups)
-    ansible_user                 = "saho"
-    ansible_ssh_private_key_file = "~/.ssh/pve"
-  }
-}
+# resource "ansible_playbook" "control_planes_common" {
+#   for_each   = ansible_host.control_planes
+#   name       = each.value.name
+#   groups     = each.value.groups
+#   playbook   = "${path.module}/../ansible/playbooks/common.yaml"
+#   replayable = true
+#   extra_vars = {
+#     ansible_host                 = each.value.name
+#     ansible_groups               = join(",", each.value.groups)
+#     ansible_user                 = "saho"
+#     ansible_ssh_private_key_file = "~/.ssh/pve"
+#   }
+# }
 
-resource "ansible_playbook" "nodes_common" {
-  for_each   = ansible_host.nodes
-  name       = each.value.name
-  groups     = each.value.groups
-  playbook   = "${path.module}/../ansible/playbooks/common.yaml"
-  replayable = true
-  extra_vars = {
-    ansible_host                 = each.value.name
-    ansible_groups               = join(",", each.value.groups)
-    ansible_user                 = "saho"
-    ansible_ssh_private_key_file = "~/.ssh/pve"
-  }
-}
+# resource "ansible_playbook" "nodes_common" {
+#   for_each   = ansible_host.nodes
+#   name       = each.value.name
+#   groups     = each.value.groups
+#   playbook   = "${path.module}/../ansible/playbooks/common.yaml"
+#   replayable = true
+#   extra_vars = {
+#     ansible_host                 = each.value.name
+#     ansible_groups               = join(",", each.value.groups)
+#     ansible_user                 = "saho"
+#     ansible_ssh_private_key_file = "~/.ssh/pve"
+#   }
+# }
