64bfcc226713accca49fe83f96d3f32015220339
Sami Honkasalo <honkasalo.sami@gmail.com>
2023-10-07 17:01:36 +0300

    Update dependencies and longhorn host

--- infra/terraform/metallb.tf
+++ infra/terraform/metallb.tf
@@ -10,21 +10,21 @@ resource "kubernetes_namespace" "metallb" {
   }
 }
 
-resource "null_resource" "apply_metallb_crds" {
+resource "helm_release" "metallb" {
   depends_on = [kubernetes_namespace.metallb]
+  name       = "metallb"
+  repository = "https://metallb.github.io/metallb"
+  chart      = "metallb"
+  version    = "0.13.11"
+  namespace  = kubernetes_namespace.metallb.metadata[0].name
+}
+
+resource "null_resource" "apply_metallb_crds" {
+  depends_on = [helm_release.metallb]
   triggers = {
     file_sha = filesha1("${path.module}/metallb-manifests.yaml")
   }
   provisioner "local-exec" {
     command = "kubectl apply -f ${path.module}/metallb-manifests.yaml"
   }
-}
-
-resource "helm_release" "metallb" {
-  depends_on = [null_resource.apply_metallb_crds]
-  name       = "metallb"
-  repository = "https://metallb.github.io/metallb"
-  chart      = "metallb"
-  version    = "0.13.11"
-  namespace  = kubernetes_namespace.metallb.metadata[0].name
 }
